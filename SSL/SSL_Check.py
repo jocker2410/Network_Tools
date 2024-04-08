25464f79ca62bddf815d09f4f3735bce80cff4ec
jocker2410 <jocker2410@gmail.com>
2024-04-08 10:04:03 +0200

    Certificate query and display revised

--- SSL/SSL_Check.py
+++ SSL/SSL_Check.py
@@ -20,18 +20,14 @@ class GetSSL:
             conn.close()
             cert_not_after = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
             exp_days = (cert_not_after - datetime.now()).days
-
             return cert_not_after, exp_days
-
         except (ssl.CertificateError, socket.error) as e:
             return None, None
 
     def _p_green(self, msg):
         print(f"{Fore.GREEN}[+] {msg}{Style.RESET_ALL}")
-
     def _p_red(self, msg):
         print(f"{Fore.RED}[-] {msg}{Style.RESET_ALL}")
-
     def _p_yellow(self, msg):
         print(f"{Fore.YELLOW}[!] {msg}{Style.RESET_ALL}")
     def _p_magenta(self, msg):
@@ -50,22 +46,21 @@ class GetSSL:
             elif exp_days < 15:
                 self._p_yellow(f"Valid until:\t\t\t{cert_not_after}")
                 self._p_yellow(f"Valid for:\t\t\t\t{exp_days} days")
-
             else:
                 self._p_red("Something went wrong, please check the debu!")
             Style.RESET_ALL
             print()
+
 def clear_screen():
   if os.name == "nt":
     os.system("cls")
   else:
     os.system("clear")
-
+      
 def main():
     websites = ["eval.testdom.com", "sploitus.com", "exploit-db.com", "vscode.dev", "perplexity.ai"]
     clear_screen()
     ssl_checker = GetSSL(websites)
     ssl_checker.check_ssl()
-
 if __name__ == '__main__':
     main()
